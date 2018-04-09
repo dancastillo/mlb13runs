b39acd1dadadee407eaf0a9ffe661769bb66bccc
dancastillo <dancastillo99@gmail.com>
2018-04-09 09:27:13 -0400

    server/index.js: added express for routing

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
+
+const {routes} = require('./routes/index');
+
 const app = express();
+const port = 3000;
+
+routes(app);
+
+app.listen(port, () => console.log(`listening on port: ${port}`));
