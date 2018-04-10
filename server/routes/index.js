736292b4b5488eab27af0d91b037b84d01f57c16
dancastillo <dancastillo99@gmail.com>
2018-04-10 15:31:31 -0400

    routes/index.js: added documentation for file

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,18 @@ const path = require('path');
 
 const get = require('./get');
 
+/**
+ * Routing for the application
+ * @param {express()} app
+ */
 const routes = (app) => {
 
+  // use the hbs templating engine
   app.set('views engine', 'hbs');
+  // direct express to views folder
   app.set('views', path.join(__dirname, './../../public/views'));
 
+  // Route all get routes to home.hbs page
   app.get('*', get.getHome);
 };
 
