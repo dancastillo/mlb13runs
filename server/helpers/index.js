9c6b6218b2615622b27399ba37af26bb9fb66753
dancastillo <dancastillo99@gmail.com>
2018-04-10 15:23:12 -0400

    helpers/index.js: created formatDate function tuned getDateFormat function
    formatDate returns array with date information

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -2,18 +2,36 @@ const apiUrl = () => {
   return `http://api.sportradar.us/mlb-t6/games/${getDateFormat()}/boxscore.json?api_key=${process.env.API_KEY}`;
 };
 
+/**
+ * @return {string}
+ * @description Returns yesterdays date
+ */
 const getDateFormat = () => {
   const date = new Date();
+  const today = formatDate(date);
+  
+  const dayBefore = new Date(today[0], today[1], today[2]-x);
+  console.log(dayBefore)
+  const yesterday = formatDate(dayBefore);
 
-  const mm = date.getMonth();
-
-  const year = date.getFullYear();
-  const yy = year.toString().substr(-2);
-
-  const day = date.getDate();
-  const dd = (day < 10) ? `0${day}` : day;
-
+  const yy = yesterday[0].toString().substr(-2);
+  const mm = yesterday[1]+1;
+  const dd = (yesterday[2] < 10) ? `0${yesterday[2]}` : yesterday[2];
+  
   return `${yy}/${mm}/${dd}`;
+};
+
+/**
+ * @param {date} date 
+ * @return {array}
+ * @description Returns array with [year, month, day]
+ */
+const formatDate = (date) => {
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const day = date.getDate();
+
+  return [year, month, day];
 };
 
 module.exports = {
