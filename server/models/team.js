e59458663d3a1242dc3234a190737e0734ebc1b1
dancastillo <dancastillo99@gmail.com>
2018-04-09 20:07:41 -0400

    team.js: added schema and attached to model for team documents

---
+++ server/models/team.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const opponentObj = {
+  name: {
+    type: String,
+    required: true
+  },
+  market: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: String,
+    required: true
+  },
+  opponentScore: {
+    type: Number,
+    require: true
+  }
+};
+
+const TeamSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  market: {
+    type: String,
+    required: true
+  },
+  // array of runs obtained
+  runs: {
+    type: [Number],
+    required: true,
+    default: []
+  },
+  scores: [
+    {
+      // score
+      score: {
+        type: Number,
+        required: true
+      },
+      // date of score
+      date: {
+        type: String,
+        required: true
+      },
+      // opponent data
+      opponent: opponentObj
+    }
+  ]
+});
+
+const Team = mongoose.model('Team', TeamSchema);
+
+module.exports = {Team}
