3ceedd469e3727cda6c1c8c9d5cbd34f5401b854
dancastillo <dancastillo99@gmail.com>
2018-04-09 20:03:19 -0400

    routes/index.js: code spliting app.get callback into its own file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,14 @@
 const hbs = require('hbs');
 const path = require('path');
 
+const get = require('./get');
+
 const routes = (app) => {
 
   app.set('views engine', 'hbs');
   app.set('views', path.join(__dirname, './../../public/views'));
 
-  app.get('/', (req, res) => {
-    res.render('home.hbs');
-  });
+  app.get('/', get.getHome);
 };
 
 module.exports = {routes};
