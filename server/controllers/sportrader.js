b96220b3ba73c3931ecc5e9718c746fe4a919692
dancastillo <dancastillo99@gmail.com>
2018-04-10 19:19:08 -0400

    sportrader.js: interacts with api and checks if api was called upon already

---
+++ server/controllers/sportrader.js
@@ -0,0 +1,87 @@
+const request = require('request');
+
+const mongoose = require('./../db/mongoose');
+const helpers = require('./../helpers/index');
+const {Team} = require('./../models/team');
+const find = require('./find');
+const update = require('./update');
+
+const saveTeamRunsData = (games, date) => {
+  // home team name
+  const name = games.game.home.name;
+  // home team market
+  const market = games.game.home.market;
+  // home team runs
+  const runs = games.game.home.runs;
+
+  // away team name
+  const awayName = games.game.away.name;
+  // away team market
+  const awayMarket = games.game.away.market;
+  // away team runs
+  const awayRuns = games.game.away.runs;
+
+  const scores = {
+    score: runs,
+    date: date,
+    opponent: {
+      name: awayName,
+      market: awayMarket,
+      data: date,
+      opponentScore: awayRuns
+    }
+  };
+
+  const awayScores = {
+    score: awayRuns,
+    date: date,
+    opponent: {
+      name: name,
+      market: market,
+      data: date,
+      opponentScore: runs
+    }
+  };
+
+    update.updateScores(team, scores);
+    update.updateScores(team, awayScores);
+}
+
+const makeApiRequest = () => {
+  // request(helpers.apiUrl, function (error, response, body) {
+    try {
+      const {data} = require('./../db/data.js');
+      // console.log(data)
+      const body = JSON.stringify(data);
+      var jsonBody = JSON.parse(body);
+  
+      let games = jsonBody['league']['games'];
+      const date = jsonBody.league.date;
+  
+      let num = 1;
+      games.forEach((games) => {
+        saveTeamRunsData(games, date);
+        num ++;
+      });
+  
+    } catch (err) {
+      console.log(`api error in sportrader: ${err}`);
+    }  
+  // });
+}
+
+/**
+ * @description Checks if api was already checked for yesterdays date
+ */
+const getDataFromAPI = () => {
+  const yesterdayDate = helpers.getDateFormat()
+
+  find.isDataUsed(yesterdayDate).then((isDataUsed) => {
+    makeApiRequest();
+  }).catch((err) => {
+    console.log(err);
+  });
+
+}
+
+getDataFromAPI();
