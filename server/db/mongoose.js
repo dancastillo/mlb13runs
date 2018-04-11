349a7f6c61912101d3e7d1293d84eca0c4fb19d7
dancastillo <dancastillo99@gmail.com>
2018-04-10 20:10:34 -0400

    mongoose.js: tuned db name

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/13runs';
+const url = 'mongodb://localhost:27017/mlb13runs';
 // const url = process.env.DB_URI;
 
 mongoose.connect(url, {}).then(() => {
