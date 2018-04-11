a704007d7741eadaaebbc9403d78c8bcdd9f7353
dancastillo <dancastillo99@gmail.com>
2018-04-10 20:03:39 -0400

    get.js: make sure data has been updated call to getDataFromAPI

--- server/routes/get.js
+++ server/routes/get.js
@@ -1,11 +1,15 @@
 const find = require('./../controllers/find');
-
+const sportrader = require('./../controllers/sportrader')
 /**
  * Route users to home page with data from collection about teams
  * @param {object} req 
  * @param {object} res 
  */
 const getHome = (req, res) => {
+  // make sure data has been updated
+  sportrader.getDataFromAPI;
+
+  // get all teams and data
   find.findAllTeams().then((teams) => {
     res.render('home.hbs', {
       teams: teams
