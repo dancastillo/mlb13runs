1d0e07b0710451cd6fd848fa1b8e91e5c53361dd
dancastillo <dancastillo99@gmail.com>
2018-04-10 20:21:16 -0400

    find.js: added isDataUsed to check if data has been updated

--- server/controllers/find.js
+++ server/controllers/find.js
@@ -1,5 +1,6 @@
 const mongoose = require('./../db/mongoose');
 const {Team} = require('./../models/team');
+const {Day} = require('./../models/Day');
 const save = require('./save');
 
 const findTeam = (name, market) => {
@@ -21,15 +22,48 @@ const findTeam = (name, market) => {
   });
 };
 
+/**
+ * 
+ */
 const findAllTeams = () => {
   return Team.find().then((teams) => {
     return teams;
   }).catch((err) => {
     return Promise.reject();
   });
-}
+};
+
+/**
+ * Checks if date data/runs have been updated
+ * @param {string} date 
+ * @return {boolean} if date was in use
+ *          true - then NEED TO update collection
+ *          falses - data has ALREADY been updated
+ */
+const isDataUsed = (date) => {
+  return Day.findOne({
+    date
+  }).then((date) => {
+    // if date doesnt exist
+    // false means runs for date havent been updated
+    if (!date) {
+      return true;
+    }
+
+    // if date.retrieved is false
+    // false means runs for date havent been updated
+    if (!date.retrieved) {
+      return true;
+    }
+
+    return false;
+  }).catch((err) => {
+    return Promise.reject();
+  });
+};
 
 module.exports = {
   findTeam,
-  findAllTeams
+  findAllTeams,
+  isDataUsed
 };
