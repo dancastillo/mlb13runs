5e1f185c31bd4f920e7638a20376ceb6f1763043
dancastillo <dancastillo99@gmail.com>
2018-04-10 20:43:59 -0400

    update.js: added updateDateRetrieved which set to true for date grab

--- server/controllers/update.js
+++ server/controllers/update.js
@@ -1,19 +1,51 @@
+const mongoose = require('./../db/mongoose');
 const {Team} = require('./../models/team');
+const {Day} = require('./../models/day');
 
+/**
+ * @description Updates scores based on team
+ * @param {object} team 
+ * @param {object} scores 
+ */
 const updateScores = (team, scores) => {
   return Team.findOneAndUpdate({
     name: team.name,
-    market: team.market
+    market: team.market,
+    abbr: team.abbr
   }, {
     $push: {
-      scores
+      scores: scores
+    },
+    $addToSet: {
+      runs: scores.score
     }
   }).then((doc) => {
-    console.log(doc);
     return doc;
   }).catch((err) => {
     return Promise.reject();
   });
-}
+};
+
+/**
+ * 
+ * @param {string} date
+ * @return Returns data object
+ */
+const updateDateRetrieved = (date) => {
+  return Day.findOneAndUpdate({
+    date
+  }, {
+    $set: {
+      retrieved: true
+    }
+  }).then((date) => {
+    if (!date.retrieved) {
+      return Promise.reject(err);
+    }
+    return date;
+  }).catch((err) => {
+    return Promise.reject(err);
+  });
+};
 
 module.exports = {updateScores};
